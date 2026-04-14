17771fd60eb23cbf3932f1c430cdeccc9b4b55e9
Kamparapu Lokesh Pavan Teja <lokeshpavantejakamparapu@gmail.com>
2026-04-14 15:27:12 +0530

    Add files via upload

---
+++ Mahasena.py
@@ -0,0 +1,16 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+even = 0
+odd = 0
+
+for i in A:
+    if i % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+if even > odd:
+    print("READY FOR BATTLE")
+else:
+    print("NOT READY")
